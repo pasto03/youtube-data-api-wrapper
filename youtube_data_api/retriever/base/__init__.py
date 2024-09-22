8f3d44696d23a6fb2bdd4ed8a0cbf8b504af29d6
pasto03 <chumaolaw@gmail.com>
2024-09-22 18:47:38 +0800

    initial commit

---
+++ youtube_data_api/retriever/base/__init__.py
@@ -0,0 +1,2 @@
+from .pipe import BasePipe
+from .params import BaseCheckboxProps, BaseParams
--- youtube_data_api/retriever/playlist_items/pipe.py
+++ youtube_data_api/retriever/playlist_items/pipe.py
@@ -2,78 +2,17 @@ from typing import TypeAlias, Literal
 from tqdm import tqdm
 import math
 
+from ..base import BasePipe
 from .params import PlaylistItemsParams
-from ...utils import flatten_chain
-
 
 RetrieveMethod: TypeAlias = Literal["head", "custom", "all"]
-
-class PlaylistItemsPipe:
+    
+    
+class PlaylistItemsPipe(BasePipe):
     """
     obtain playlistItems by a playlistId
     Only PlaylistItemsWorker is supposed to implement this object
     """
-    def __init__(self, params: PlaylistItemsParams, playlist_items_fn=None, 
+    def __init__(self, params: PlaylistItemsParams, pipe_fn=None, 
                  retrieval: RetrieveMethod = "head", n=10):
-        self.params = params
-        self.playlist_items_fn = playlist_items_fn
-        self.retrieval = retrieval
-        self.n = n
-        self.hard_limit = 50
-        
-    def _get_response(self, pageToken=None, n=None) -> dict:
-        if not self.playlist_items_fn:
-            raise ValueError("Do not implement this object explicitly. Use PlaylistItemsWorker instead.")
-        
-        self.params.pageToken = pageToken
-        self.params.maxResults = 50 if not n else n
-#         print(self.params)
-        request = self.playlist_items_fn.list(**self.params.to_dict())
-        response = request.execute()
-        return response
-    
-    def _get_all_response(self) -> list[dict]:
-        # print("inside _get_all_response")
-        all_response = list()
-        first_response = self._get_response()
-        nextPageToken = first_response.get("nextPageToken")
-        # print("nextPageToken: {}".format(nextPageToken))
-        page_info = first_response['pageInfo']
-        num_page = math.ceil(page_info['totalResults'] / page_info['resultsPerPage'])
-        if not nextPageToken:
-            return [first_response]
-        
-        all_response.append(first_response)
-#         print("num pages:", num_page)
-        bar = tqdm(total=num_page, desc="fetching response items...")
-        bar.update()   # first batch already obtained
-        count = 0
-        
-        while nextPageToken:
-            response = self._get_response(pageToken=nextPageToken)
-            all_response.append(response)
-            nextPageToken = response.get("nextPageToken")
-#             print("Current count:", count)
-#             print("nextPageToken: {}".format(nextPageToken))
-            bar.update()
-            count += 1
-        
-        all_response = flatten_chain([i["items"] for i in all_response])
-        bar.set_description("{} playlistItems obtained.".format(len(all_response)))
-        bar.close()
-        
-        return all_response
-    
-    def run_pipe(self) -> list[dict] | None:
-        """call API and return list of playlist items"""
-        if self.retrieval == "head":
-            return self._get_response().get("items")
-        
-        elif self.retrieval == "custom":
-            n = self.n
-            assert n > 0 and type(n) == int, "only positive integer allowed"
-            n = min(n, self.hard_limit)
-            return self._get_response(n=n).get("items")
-        
-        elif self.retrieval == "all":
-            return self._get_all_response()
+        super().__init__(params=params, pipe_fn=pipe_fn, retrieval=retrieval, n=n)
